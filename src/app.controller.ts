ba248641088d784adb85c8be9b701a05bbf7b7b0
myflowpl <pedro.blaszczak@gmail.com>
2021-07-26 13:03:44 +0200

    swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
+import { UsersService } from './users/services';
 
+@ApiTags('app')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+
+  constructor(
+    private readonly appService: AppService,
+    private usersService: UsersService
+    ) {}
 
   @Get()
   getHello(): string {
+    //
     return this.appService.getHello();
   }
 }
+
+// /
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,32 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { expressApp } from './express/server';
 import { ExpressAdapter, NestExpressApplication } from "@nestjs/platform-express";
+import { SwaggerModule, DocumentBuilder, SwaggerDocumentOptions, SwaggerCustomOptions } from '@nestjs/swagger';
 
 const expressAdapter = new ExpressAdapter(expressApp);
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, expressAdapter);
+
+  // SWAGGER SETUP
+  const config = new DocumentBuilder()
+    .setTitle('Mój Projekt w Nest')
+    .setDescription('Przykładowy projekt w Node.js i TypeScript')
+    .setVersion('1.0')
+    .build();
+
+  const options: SwaggerDocumentOptions =  {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: { persistAuthorization: true },
+  };
+
+  SwaggerModule.setup('docs', app, document, customOptions);
+  // END OF SWAGGER SETUP
 
   await app.listen(3000);
 }
