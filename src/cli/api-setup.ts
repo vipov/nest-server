fdcdc7ab23804077ac46913cefc5041f6893eb4d
myflowpl <pedro.blaszczak@gmail.com>
2021-07-30 14:00:32 +0200

    cli

---
+++ src/cli/api-setup.ts
@@ -0,0 +1,59 @@
+import { existsSync, readFileSync, writeFileSync } from 'fs'
+import * as chalk from 'chalk';
+import * as inquirer from 'inquirer';
+
+(async function setup() {
+  const envFile = "./.env";
+  const envTplFile = './.env-tpl';
+
+  if(existsSync(envFile)) {
+    return console.log(chalk.green('This project is ready :)'))
+  }
+  console.log(chalk.green('Setting up...'))
+
+  let tpl = readFileSync(envTplFile).toString();
+
+  const data = await inquirer.prompt([
+    {
+      type: 'text',
+      name: 'jwt_secret',
+      message: 'Select your jwt secret',
+      default: 'mySecret',
+      validate: (v) => !!v
+    },
+    {
+      type: 'text',
+      name: 'worker_port',
+      message: 'Worker microservice port number',
+      default: 3001,
+      validate: (v) => parseInt(v, 10) >=0
+    },
+    {
+      type: 'text',
+      name: 'storage_dir',
+      message: 'Where is your storage mounted',
+      default: './storage',
+      validate: (v) => !!v
+    },
+  ]);
+
+  var Table = require('cli-table');
+
+  var table = new Table({
+      head: ['Key', 'Value']
+  });
+
+  for (const key in data) {
+    if (Object.prototype.hasOwnProperty.call(data, key)) {
+      const value = data[key];
+      table.push([key, value]);
+      tpl = tpl.replace(`<${key}>`, value)
+    }
+  }
+
+  console.log(table.toString());
+
+  writeFileSync(envFile, tpl);
+
+  console.log(chalk.green('You are ready to go :)'))
+})()
---
+++ src/cli/api.ts
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+
+clear();
+console.log('')
+console.log(chalk.yellow(figlet.textSync('My API CLI', { horizontalLayout: 'full' })));
+
+program
+    .version('0.0.1', '--version')
+    .description('Mange the infrastructure of My API')
+    .command('setup', 'setup this project for development')
+    .command('deploy', 'manage deploys')
+    .command('stats', 'display system stats', { isDefault: false });
+
+
+// allow commander to parse `process.argv`
+program.parse(process.argv);
+
+// if no command display help info
+if (program.args.length === 0) {
+  program.help();
+}
