713b2c98864c994ee983859c3f1d6e51fd71dca2
myflowpl <pedro.blaszczak@gmail.com>
2021-12-06 14:56:34 +0100

    express app + integration

---
+++ src/express/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+
+const app = express()
+
+// app.get('/', function (req, res) {
+//   res.send('Hello from Express.js')
+// })
+
+app.get('/user', 
+  function (req, res, next) {    // logger middleware
+    console.log(req.url);
+    next();
+  },
+  function (req, res, next) {    // guard middleware
+    req.user = req.query.name;
+    (req.user) ? next() : next(new Error('forbidden'));
+  },
+  function (req, res) {          // request handler
+    res.send('Hi ' + req.user)
+  },
+  function (err, req, res, next) {    // error handler
+    res.json({status: 500, err: err.message})
+  }
+)
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+  // do your logic
+  res.json({params: req.params});
+
+})
+
+
+exports.expressApp = app;
+
+// app.listen(3000, () => console.log(`Example app listening on port 3000!`));
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { expressApp } from "./express/server";
+import { ExpressAdapter, NestExpressApplication } from "@nestjs/platform-express";
+
+const expressAdapter = new ExpressAdapter(expressApp);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, expressAdapter);
   await app.listen(3000);
-}
+} 
 bootstrap();
