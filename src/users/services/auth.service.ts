d9764e3f543e7943916a833fb5c38a0b1b3eec92
myflowpl <pedro.blaszczak@gmail.com>
2021-12-07 17:03:30 +0100

    login: auth service

--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { RequestPayload, TokenPayload, User } from '../entities/user.entity';
+import { UsersService } from './users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async findByCredentials(email: string, password: string): Promise<User | null> {
+    
+    return null; // TODO
+  }
+
+  async encodeUserToken(user: User): Promise<string> {
+
+    const payload: TokenPayload = {username: user.email, sub: user.id};
+
+    return this.jwtService.sign(payload);
+  }
+
+  async decodeUserToken(token: string): Promise<RequestPayload | null> {
+
+    const payload: TokenPayload = this.jwtService.verify(token);
+
+    const [user] = await this.usersService.findBy({id: payload.sub});
+
+    return user ? {user, token} : null;
+  }
+
+
+
+}
