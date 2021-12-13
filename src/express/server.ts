3993677321c5d5ebe81ba0740aa480fd46b6d711
myflowpl <pedro.blaszczak@gmail.com>
2021-12-13 14:26:47 +0100

    format & lint

--- src/express/server.ts
+++ src/express/server.ts
@@ -1,37 +1,40 @@
-const express = require('express')
-import { Request } from "express";
-
-
+const express = require('express');
+import { Request } from 'express';
 
 const app = express();
 
 const users = [];
 
 app.get('/', function (req: Request, res) {
-  res.send('Hello from Express.js')
-})
+  res.send('Hello from Express.js');
+});
 
-app.get('/user', 
-  function (req: Request, res, next) {    // logger middleware
+app.get(
+  '/user',
+  function (req: Request, res, next) {
+    // logger middleware
     console.log(req.url);
     next();
   },
-  function (req: Request, res, next) {    // guard middleware
+  function (req: Request, res, next) {
+    // guard middleware
     req.userName = req.query.name as string;
     users.push(req.userName);
-    (req.userName) ? next() : next(new Error('forbidden'));
+    req.userName ? next() : next(new Error('forbidden'));
   },
-  function (req: Request, res) {          // request handler
-    console.log('USERS', users)
-    res.send('Hi ' + req.userName)
+  function (req: Request, res) {
+    // request handler
+    console.log('USERS', users);
+    res.send('Hi ' + req.userName);
   },
-  function (err, req, res, next) {    // error handler
-    res.json({status: 500, err})
-  }
-)
+  function (err, req, res, next) {
+    // error handler
+    res.json({ status: 500, err });
+  },
+);
 
 exports.expressApp = app;
 
-export {app as expressApp};
+export { app as expressApp };
 
 // app.listen(3000, () => console.log(`Example app listening on port 3000!`));
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
+import {
+  ExpressAdapter,
+  NestExpressApplication,
+} from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { expressApp } from './express/server';
 
 const expressAdapter = new ExpressAdapter(expressApp);
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, expressAdapter);
+  const app = await NestFactory.create<NestExpressApplication>(
+    AppModule,
+    expressAdapter,
+  );
 
   await app.listen(3000);
 }
