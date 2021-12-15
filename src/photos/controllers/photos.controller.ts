00cc993d89450370a81088ce642685b3a3e9e8d5
myflowpl <pedro.blaszczak@gmail.com>
2021-12-15 12:57:03 +0100

    uplload + thumbs

--- src/photos/controllers/photos.controller.ts
+++ src/photos/controllers/photos.controller.ts
@@ -8,15 +8,22 @@ import {
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ApiBody, ApiConsumes, ApiTags } from '@nestjs/swagger';
 import { FileUploadDto } from '../dto/photos.dto';
+import { PhotosService } from '../services/photos.service';
 
 @Controller('photos')
 @ApiTags('Photos')
 export class PhotosController {
+  constructor(private photosService: PhotosService) {}
+
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   @ApiConsumes('multipart/form-data')
   @ApiBody({ type: FileUploadDto })
-  upload(@UploadedFile() file: Express.Multer.File, @Body() body) {
-    return { file, body };
+  async upload(@UploadedFile() file: Express.Multer.File, @Body() body) {
+    const photo = await this.photosService.create(file);
+
+    const thumbs = await this.photosService.createThumbs(photo.filename);
+
+    return { file, body, photo, thumbs };
   }
 }
--- src/photos/services/photos.service.ts
+++ src/photos/services/photos.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { extname, join } from 'path';
+import { ConfigService } from '../../config';
+import { rename } from 'fs/promises';
+import { createHash } from 'crypto';
+import * as sharp from 'sharp';
 
 @Injectable()
-export class PhotosService {}
+export class PhotosService {
+  constructor(private config: ConfigService) {}
+
+  async create(file: Express.Multer.File) {
+    const ext = extname(file.originalname).toLowerCase();
+
+    const filename = createHash('md5').update(file.path).digest('hex') + ext;
+
+    const destFile = join(this.config.STORAGE_PHOTOS, filename);
+
+    await rename(file.path, destFile);
+
+    const photo = {
+      filename,
+    };
+
+    return photo;
+  }
+
+  async createThumbs(filename: string) {
+    const srcFile = join(this.config.STORAGE_PHOTOS, filename);
+    const destFile = join(this.config.STORAGE_THUMBS, filename);
+
+    await sharp(srcFile)
+      .rotate()
+      .resize(200, 200, { fit: 'cover', position: 'attention' })
+      .jpeg({ quality: 100 })
+      .toFile(destFile);
+
+    return {
+      small: destFile,
+    };
+  }
+}
