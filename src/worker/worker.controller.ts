be52d1fbf1215fdb148a1ac51532760ed5577a3a
myflowpl <pedro.blaszczak@gmail.com>
2021-12-16 16:35:32 +0100

    app client on worker

--- src/worker/worker.controller.ts
+++ src/worker/worker.controller.ts
@@ -1,11 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern } from '@nestjs/microservices';
+import { PhotosApi } from '../app-client/api';
 import { PhotosService } from '../photos/services/photos.service';
 import { CreateThumbsEvent, SumEvent } from '../worker-client';
 
 @Controller('worker')
 export class WorkerController {
-  constructor(private photosService: PhotosService) {}
+  constructor(
+    private photosService: PhotosService,
+    private phtosApi: PhotosApi,
+  ) {}
 
   @MessagePattern(SumEvent.message)
   accumulate(e: SumEvent): number {
@@ -16,6 +20,8 @@ export class WorkerController {
   async createThumbs(e: CreateThumbsEvent) {
     console.log('CREATE THUMBS', e);
     const thumbs = await this.photosService.createThumbs(e.filename);
+
+    await this.phtosApi.thumbsCreated(thumbs);
 
     return thumbs;
   }
--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { config } from 'dotenv';
+import { PhotosApi } from '../app-client/api';
+import { ConfigModule, ConfigService } from '../config';
 import { PhotosModule } from '../photos/photos.module';
 import { WorkerController } from './worker.controller';
 
 @Module({
-  imports: [PhotosModule],
+  imports: [PhotosModule, ConfigModule],
   controllers: [WorkerController],
+  providers: [
+    {
+      provide: PhotosApi,
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return new PhotosApi({ basePath: config.DOMAIN });
+      },
+    },
+  ],
 })
 export class WorkerModule {}
