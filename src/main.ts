69b0f7f22aded8e7ffa6dab2dc25f6631bf70ad9
myflowpl <pedro.blaszczak@gmail.com>
2022-02-14 15:48:27 +0100

    nest + express

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  ExpressAdapter,
-  NestExpressApplication,
-} from '@nestjs/platform-express';
+import { ExpressAdapter, NestExpressApplication } from '@nestjs/platform-express';
 import { expressApp } from './express/server';
 
 const expressAdapter = new ExpressAdapter(expressApp);
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(
-    AppModule,
-    expressAdapter,
-  );
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, expressAdapter);
   await app.listen(3000);
 }
 bootstrap();
