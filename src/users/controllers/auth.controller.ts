0c47cde264410f837d99ba68658b84023a3df67b
myflowpl <pedro.blaszczak@gmail.com>
2022-02-15 16:57:21 +0100

    login

--- src/users/controllers/auth.controller.ts
+++ src/users/controllers/auth.controller.ts
@@ -1,16 +1,33 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Auth } from '../decorators/auth.decorator';
 import { Payload } from '../decorators/payload.decorator';
+import { AuthLoginDto, AuthLoginResponse } from '../dto/auth.dto';
 import { User } from '../entities/user.entity';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { AuthService } from '../services/auth.service';
 
 @Controller('auth')
 @ApiTags('Auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Get('me')
   @UseGuards(JwtAuthGuard)
   getMe(@Payload('user') user: User) {
     return { user };
+  }
+
+  @Post('login')
+  async login(@Body() data: AuthLoginDto): Promise<AuthLoginResponse> {
+    const user = await this.authService.validateUser(data.email, data.password);
+
+    if (!user) {
+      throw new UnauthorizedException('Credentails Invalid');
+    }
+
+    const token = await this.authService.encodeUserToken(user);
+
+    return { token, user };
   }
 }
--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -1,4 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { RequestPayload, TokenPayload, User } from '../entities/user.entity';
+import { UsersService } from './users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService, private jwtService: JwtService) {}
+
+  async validateUser(email: string, password: string): Promise<User | null> {
+    const [user] = await this.usersService.findBy({ email });
+
+    if (!user) {
+      return null;
+    }
+
+    const isValid = await this.validatePassword(password, user.password);
+
+    if (!isValid) {
+      return null;
+    }
+
+    return user;
+  }
+
+  async encodePassword(password: string): Promise<string> {
+    // TODO use bcrypt library
+    return password;
+  }
+
+  async validatePassword(password: string, hash: string): Promise<boolean> {
+    // TODO use bcrypt library
+    return password === hash;
+  }
+
+  async encodeUserToken(user: User): Promise<string> {
+    const payload: TokenPayload = { username: user.email, sub: user.id };
+
+    return this.jwtService.sign(payload);
+  }
+
+  async decodeUserToken(token: string): Promise<RequestPayload> {
+    const payload: TokenPayload = this.jwtService.verify(token);
+
+    const user = await this.usersService.findOne(payload.sub);
+
+    return user ? { user } : null;
+  }
+}
