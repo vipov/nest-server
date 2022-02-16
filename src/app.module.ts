f9bc56bd081c036408b990eed0bbce6df3dfed59
myflowpl <pedro.blaszczak@gmail.com>
2022-02-16 13:00:53 +0100

    upload wip

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from './config/config.module';
+import { PhotosModule } from './photos/photos.module';
 
 @Module({
-  imports: [UsersModule, ConfigModule],
+  imports: [UsersModule, ConfigModule, PhotosModule],
   controllers: [AppController],
   providers: [AppService],
 })
