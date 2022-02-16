b6305ca84528cf0ad8b95185d7039997fd33ed09
myflowpl <pedro.blaszczak@gmail.com>
2022-02-16 13:21:27 +0100

    wip

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -11,6 +11,16 @@ export class ConfigService implements OnModuleInit, OnModuleDestroy {
   readonly STORAGE_DIR = resolve(process.env.STORAGE_DIR);
   readonly JWT_SECRET = process.env.JWT_SECRET;
 
+  readonly STORAGE_TMP = resolve(this.STORAGE_DIR, 'tmp');
+  readonly STORAGE_PHOTOS = resolve(this.STORAGE_DIR, 'photos');
+
+  readonly STORAGE_ASSETS = resolve(this.STORAGE_DIR, 'assets');
+  readonly STORAGE_THUMBS = resolve(this.STORAGE_ASSETS, 'thumbs');
+
+  readonly PHOTOS_DOMAIN = 'http://localhost:3000';
+  readonly PHOTOS_BASE_PATH = joinUrl(this.PHOTOS_DOMAIN, 'thumbs');
+  readonly PHOTOS_DOWNLOAD_PATH = joinUrl(this.PHOTOS_DOMAIN, 'photos/download');
+
   async onModuleInit() {
     // TODO implement validation & transformation of the config
   }
