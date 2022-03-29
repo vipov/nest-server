ffad3c601de47c81ca0bcd6dfc6a3dd5da0401a5
myflowpl <pedro.blaszczak@gmail.com>
2022-03-29 17:00:57 +0200

    photos upload

--- src/photos/controllers/photos.controller.ts
+++ src/photos/controllers/photos.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiConsumes, ApiTags } from '@nestjs/swagger';
+import { PhotoUploadDto } from '../dto/photos.dto';
+import { PhotosService } from '../services/photos.service';
 
 @Controller('photos')
-export class PhotosController {}
+@ApiTags('Photos')
+export class PhotosController {
+
+  constructor(
+    private photosService: PhotosService,
+  ) {}
+
+  @Post('upload')
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(FileInterceptor('file'))
+  async upload(@UploadedFile() file: Express.Multer.File, @Body() data: PhotoUploadDto) {
+
+    const photo = await this.photosService.create(file);
+
+    const thumbs = await this.photosService.createThumbs(photo.filename);
+
+    return { file, data, photo, thumbs }
+  }
+}
--- src/photos/services/photos.service.ts
+++ src/photos/services/photos.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { createHash } from 'crypto';
+import { rename } from 'fs/promises';
+import { extname, join } from 'path';
+import { ConfigService } from '../../config';
 
 @Injectable()
-export class PhotosService {}
+export class PhotosService {
+
+  constructor(
+    private config: ConfigService,
+  ) {}
+
+  async create(file: Express.Multer.File) {
+    // TODO validate if this is a image file
+
+    // create new file name
+    const ext = extname(file.originalname).toLowerCase();
+    const filename = createHash('md5').update(file.path).digest('hex') + ext;
+
+    // move from tmp to storage
+    const destFile = join(this.config.STORAGE_PHOTOS, filename);
+    await rename(file.path, destFile);
+
+    // TODO create database record
+
+    return { filename }
+  }
+
+  async createThumbs(filename: string) {
+
+    return {}
+  }
+}
