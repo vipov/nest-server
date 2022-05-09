c922318245d0b74f0a86fbae09ff44f35d76a699
myflowpl <pedro.blaszczak@gmail.com>
2022-05-09 14:34:09 +0200

    express

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,21 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+
   constructor(private readonly appService: AppService) {}
 
+  id: number;
+  
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(id: undefined | number): string {
+
+    if(id) {
+      id.toString();
+    }
+    // id.toString();
+
+    const data: string =  this.appService.getHello();
+
+    return data;
   }
 }
