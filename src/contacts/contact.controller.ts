f5700f0bf9cdca5373fe04df45fcd23680cb3dff
Przemek P <przemyslaw.pramik@rtbhouse.com>
2022-05-10 10:38:55 +0200

    before adding pipes

--- src/contacts/contact.controller.ts
+++ src/contacts/contact.controller.ts
@@ -1,37 +1,74 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { Contact } from './contact.entity';
+import {
+  GetContactsDto,
+  CreateContactDto,
+  UpdateContactDto,
+  UpdateContactResponse,
+} from './contact.dto';
 
 @Controller('contacts')
 @ApiTags('Contacts')
 export class ContactController {
-
   @Get()
-  async findAll(): Promise<Contact[]> {
-
-    // todo pobrac rekordy i je zwrocic
+  async findAll(@Query() query: GetContactsDto): Promise<Contact[]> {
+    // TODO pobrac rekordy i je zwrocic
     return [
-      {id: 1, name: 'piotr', email: 'pio@myflow.pl', message: 'test'},
-      {id: 2, name: 'piotr', email: 'pio@myflow.pl', message: 'test'},
-    ]
+      { id: 1, name: 'przemek', email: 'pp@myflow.pl', message: 'test' },
+      { id: 2, name: 'przemek', email: 'pp@myflow.pl', message: 'test' },
+      query as any,
+    ];
   }
 
   @Get(':id')
   async findOne(@Param('id') id: string): Promise<Contact> {
+    // TODO uzyc bazy danych do odczytu rekordu dla id
 
-    //TODO uzyc bazy danych do odczytu rekordu dla id
-    
     return {
       id: parseInt(id, 10),
       email: '',
       name: '',
       message: '',
-    }
+    };
   }
 
-  create() {}
+  @Post()
+  async create(@Body() data: CreateContactDto): Promise<Contact> {
+    return {
+      email: '',
+      message: 'test new one',
+      name: '',
+      ...data,
+      id: 1,
+    };
+  }
 
-  update() {}
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() data: UpdateContactDto,
+  ): Promise<UpdateContactResponse> {
+    return {
+      contact: {
+        ...data,
+        id: parseInt(id, 10),
+        email: '',
+      },
+    };
+  }
 
-  remove() {}
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<number> {
+    return parseInt(id, 10);
+  }
 }
--- src/contacts/contact.dto.ts
+++ src/contacts/contact.dto.ts
@@ -1,1 +1,42 @@
-export class ContactDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Contact } from './contact.entity';
+
+export enum SortDir {
+  ASC = 'asc',
+  DESC = 'desc',
+}
+
+export class GetContactsDto {
+  page?: number;
+  pageSize?: number;
+  sortBy?: string;
+  sortDir?: SortDir;
+}
+
+export class CreateContactDto {
+  @ApiProperty({
+    description: 'Podaj imie',
+    example: 'Przemek',
+  })
+  name: string;
+  @ApiProperty({
+    description: 'Podaj email',
+    example: 'Przemek@przemek.pl',
+  })
+  email: string;
+  @ApiProperty({
+    description: 'W czym pomoc?',
+    example: 'nice app',
+  })
+  message: string;
+  
+}
+
+export class UpdateContactDto {
+  name: string;
+  message: string;
+}
+
+export class UpdateContactResponse {
+  contact: Contact;
+}
