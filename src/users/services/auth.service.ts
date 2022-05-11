67d9465791c58e16d26a2fbf4ce1b08b74ffa84a
myflowpl <pedro.blaszczak@gmail.com>
2022-05-11 15:58:36 +0200

    register

--- src/users/services/auth.service.ts
+++ src/users/services/auth.service.ts
@@ -1,19 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { RequestPayload } from '../entities/user.entity';
+import { RequestPayload, TokenPayload, User } from '../entities/user.entity';
 import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
-  async validateUser(email: string, password: string): Promise<User | null> {}
+  constructor(
+    private usersService: UsersService, 
+    private jwtService: JwtService
+  ) {}
 
-  async encodePassword(password: string): Promise<string> {}
+  async validateUser(email: string, password: string): Promise<User | null> {
 
-  async validatePassword(password: string, hash: string): Promise<boolean> {}
+    const [user] = await this.usersService.findBy({email});
 
-  async encodeUserToken(user: User): Promise<string> {}
+    if(!user) {
+      return null;
+    }
 
-  async decodeUserToken(token: string): Promise<RequestPayload> {}
+    const isValid = await this.validatePassword(password, user.password);
+
+    return isValid ? user : null;
+  }
+
+  async encodePassword(password: string): Promise<string> {
+    return bcrypt.hash(password, 10);
+  }
+
+  async validatePassword(password: string, hash: string): Promise<boolean> {
+    return bcrypt.compare(password, hash);
+  }
+
+  async encodeUserToken(user: User): Promise<string> {
+
+    const payload: TokenPayload = { sub: user.id, username: user.email };
+
+    return this.jwtService.signAsync(payload);
+  }
+
+  async decodeUserToken(token: string): Promise<RequestPayload> {
+    
+    const payload: TokenPayload = await this.jwtService.verifyAsync(token);
+
+    const user = await this.usersService.findOne(payload.sub);
+
+    return user ? { user } : null;
+  }
 }
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,18 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { StorageService } from '../../storage/storage.service';
 import { User } from '../entities/user.entity';
 
 @Injectable()
 export class UsersService {
+
+  constructor(
+    private storage: StorageService,
+  ) {}
   
   async create(createUserDto: Partial<User>): Promise<User> {
-    
+    return this.storage.create(User, createUserDto)
   }
 
   async findBy(query: Partial<User>): Promise<User[]> {
-    
+    return this.storage.findAll(User).then(
+      users => users.filter(user => user.email === query.email)
+    )
   }
 
   async findOne(id: number): Promise<User | null> {
-    
+    return this.storage.findOne(User, id);
   }
 }
